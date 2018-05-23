4328a78aaf95996e7780fa9b3fdbe593dec8b041
PHILIP ROBINSON <philip.robinson@lausd.net>
2018-05-23 14:12:21 -0700

    Final Ver

--- Phone_Book_T8-5/Form1.Designer.cs
+++ Phone_Book_T8-5/Form1.Designer.cs
@@ -28,13 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.nameListBox = new System.Windows.Forms.ListBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.phoneLabel = new System.Windows.Forms.TextBox();
+            this.exitButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 25);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select a Name:";
+            // 
+            // nameListBox
+            // 
+            this.nameListBox.FormattingEnabled = true;
+            this.nameListBox.Location = new System.Drawing.Point(24, 41);
+            this.nameListBox.Name = "nameListBox";
+            this.nameListBox.Size = new System.Drawing.Size(120, 121);
+            this.nameListBox.TabIndex = 1;
+            this.nameListBox.SelectedIndexChanged += new System.EventHandler(this.nameListBox_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(180, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(78, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Phone Number";
+            // 
+            // phoneLabel
+            // 
+            this.phoneLabel.Enabled = false;
+            this.phoneLabel.Location = new System.Drawing.Point(172, 94);
+            this.phoneLabel.Name = "phoneLabel";
+            this.phoneLabel.Size = new System.Drawing.Size(100, 20);
+            this.phoneLabel.TabIndex = 3;
+            // 
+            // exitButton
+            // 
+            this.exitButton.Location = new System.Drawing.Point(111, 179);
+            this.exitButton.Name = "exitButton";
+            this.exitButton.Size = new System.Drawing.Size(75, 23);
+            this.exitButton.TabIndex = 4;
+            this.exitButton.Text = "Exit";
+            this.exitButton.UseVisualStyleBackColor = true;
+            this.exitButton.Click += new System.EventHandler(this.exitButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(303, 226);
+            this.Controls.Add(this.exitButton);
+            this.Controls.Add(this.phoneLabel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.nameListBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox nameListBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox phoneLabel;
+        private System.Windows.Forms.Button exitButton;
     }
 }
 
